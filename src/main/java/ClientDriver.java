f419af3eec87b776dbb983d063d5cfb2af6027b7
dingfan <zhaodingfanhaha@gmail.com>
2018-10-22 10:39:33 +0800

    Convert normal project to maven project and add client driver skeleton

---
+++ src/main/java/ClientDriver.java
@@ -0,0 +1,7 @@
+import com.mongodb.connection.Cluster;
+import com.mongodb.session.SessionContext;
+
+public class ClientDriver {
+    public Cluster cluster;
+    public SessionContext sessionContext;
+}
