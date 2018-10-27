177173fba24b42ca03d741edc9fc9a6ed476ed9e
dingfan <zhaodingfanhaha@gmail.com>
2018-10-27 20:27:36 +0800

    Update scripts for importing data to MongoDB

--- importData.sh
+++ importData.sh
@@ -1,4 +1,12 @@
-#!/usr/bin/env bash
-
 /temp/mongodb-linux-x86_64-rhel70-4.0.3/bin/mongo 192.168.48.219:27017 loadData.js
-/temp/mongodb-linux-x86_64-rhel70-4.0.3/bin/mongoimport --host 192.168.48.219 --port 27017 --db test --collection stockMiscell --type json --file ~/jsonProcess/stock_miscellaneous.json
+/temp/mongodb-linux-x86_64-rhel70-4.0.3/bin/mongoimport --host 192.168.48.219 --port 27017 --db cs4224 --collection stockMiscell --type json --file ~/jsonProcess/stock_miscellaneous.json
+
+/temp/mongodb-linux-x86_64-rhel70-4.0.3/bin/mongoimport --host 192.168.48.219 --port 27017 --db cs4224 --collection item --type json --file ~/jsonProcess/item.json
+
+/temp/mongodb-linux-x86_64-rhel70-4.0.3/bin/mongoimport --host 192.168.48.219 --port 27017 --db cs4224 --collection order --type json --file ~/jsonProcess/order.json
+
+/temp/mongodb-linux-x86_64-rhel70-4.0.3/bin/mongoimport --host 192.168.48.219 --port 27017 --db cs4224 --collection warehouse_district_customer --type json --file ~/jsonProcess/warehouse_district_customer.json
+
+/temp/mongodb-linux-x86_64-rhel70-4.0.3/bin/mongoimport --host 192.168.48.219 --port 27017 --db cs4224 --collection warehouse_stock --type json --file ~/jsonProcess/warehouse_stock.json
+
+/temp/mongodb-linux-x86_64-rhel70-4.0.3/bin/mongoimport --host 192.168.48.219 --port 27017 --db cs4224 --collection customerMiscell --type json --file ~/jsonProcess/customer_miscellaneous.json
