05229e8ffd47cb183eea037ef9ba95b01df4b483
dingfan <zhaodingfanhaha@gmail.com>
2018-10-31 01:11:14 +0800

    Add completed Transaction 3

--- src/main/java/Transaction3.java
+++ src/main/java/Transaction3.java
@@ -1,4 +1,16 @@
+import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.model.Sorts;
+import org.bson.Document;
+
+import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.mongodb.client.model.Filters.and;
+import static com.mongodb.client.model.Filters.eq;
+import static com.mongodb.client.model.Updates.set;
+
 
 public class Transaction3 {
     private MongoDatabase mongoDatabase;
@@ -18,5 +30,32 @@ public class Transaction3 {
     }
 
     public void execute() {
+        MongoCollection<Document> orderCollection = mongoDatabase.getCollection("order");
+        MongoCollection<Document> customerCollection = mongoDatabase.getCollection("customer");
+
+        for (int i = 1; i <= 10; i++) {
+            Document order = orderCollection.find(and(eq("o_w_id", W_ID), eq("o_d_id", i), eq("o_carrier_id", -1)))
+                    .sort(Sorts.ascending("o_id")).first();
+
+            // update o_carrier_id
+            int o_id = order.getInteger("o_id");
+            orderCollection.updateOne(and(eq("o_w_id", W_ID), eq("o_d_id", i), eq("o_id", o_id)), set("o_carrier_id", CARRIER_ID));
+
+            // update order o_delivery_d
+            orderCollection.updateOne(and(eq("o_w_id", W_ID), eq("o_d_id", i), eq("o_id", o_id)), set("o_delivery_d", new Timestamp(System.currentTimeMillis()).toString()));
+
+            // increment c_balance and c_delivery_cnt
+            int c_id = order.getInteger("o_c_id");
+            List<Document> order_lines = (ArrayList<Document>) order.get("order_lines");
+            double ol_amount_sum = 0;
+            for (Document order_line : order_lines) {
+                ol_amount_sum += order_line.getDouble("ol_amount");
+            }
+
+            Document customer = customerCollection.find(and(eq("c_w_id", W_ID), eq("c_d_id", i), eq("c_id", c_id))).first();
+
+            customerCollection.updateOne(and(eq("c_w_id", W_ID), eq("c_d_id", i), eq("c_id", c_id)), set("c_balance", customer.getDouble("c_balance")+ol_amount_sum));
+            customerCollection.updateOne(and(eq("c_w_id", W_ID), eq("c_d_id", i), eq("c_id", c_id)), set("c_delivery_cnt", customer.getInteger("c_delivery_cnt")+1));
+        }
     }
 }
