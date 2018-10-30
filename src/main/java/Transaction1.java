46513cb2c992d66360cc9fd63b922a1f2cc86589
LAPTOP-ThinkPad\Jiang Chunhui <jiangchunhui007@gmail.com>
2018-10-30 13:47:41 +0800

    add Xact1

--- src/main/java/Transaction1.java
+++ src/main/java/Transaction1.java
@@ -1,58 +1,45 @@
 import java.sql.Timestamp;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 import org.bson.Document;
-import org.bson.types.ObjectId;
-
-import javax.print.Doc;
 
 import static com.mongodb.client.model.Filters.and;
 import static com.mongodb.client.model.Filters.eq;
-import static com.mongodb.client.model.Projections.fields;
-import static com.mongodb.client.model.Projections.include;
-import static com.mongodb.client.model.Updates.combine;
 import static com.mongodb.client.model.Updates.set;
 
 public class Transaction1 {
     private MongoDatabase mongoDatabase;
-    private String W_ID;
-    private String D_ID;
-    private String C_ID;
+    private int W_ID;
+    private int D_ID;
+    private int C_ID;
     private int num_items;
     private int[] item_number;
     private int[] supplier_warehouse;
     private int[] quantity;
+
+    private double warehouseTax, districtTax, discount;
+    private String customerCredit;
     private String[] itemName;
     private double[] itemPrice;
-    private int[] stockQuantity;
+    private double[] stockQuantity;
     private Timestamp timestamp;
-    MongoCollection<Document> warehouseCollection;
-    MongoCollection<Document> stockCollection;
-    MongoCollection<Document> itemCollection;
-    MongoCollection<Document> distInfoCollection;
-    MongoCollection<Document> orderCollection;
-
 
     public Transaction1(MongoDatabase mongoDatabase, int W_ID, int D_ID, int C_ID, int num_items,
                         int[] item_number, int[] supplier_warehouse, int[] quantity) {
         this.mongoDatabase = mongoDatabase;
-        this.W_ID = String.valueOf(W_ID);
-        this.D_ID = String.valueOf(D_ID);
-        this.C_ID = String.valueOf(C_ID);
+        this.W_ID = W_ID;
+        this.D_ID = D_ID;
+        this.C_ID = C_ID;
         this.num_items = num_items;
         this.item_number = item_number;
         this.supplier_warehouse = supplier_warehouse;
         this.quantity = quantity;
         itemName = new String[num_items];
         itemPrice = new double[num_items];
-        stockQuantity = new int[num_items];
-        warehouseCollection = mongoDatabase.getCollection("warehouse_district_customer");
-        stockCollection = mongoDatabase.getCollection("warehouse_stock");
-        itemCollection = mongoDatabase.getCollection("item");
-        distInfoCollection = mongoDatabase.getCollection("stockMiscell");
-        orderCollection = mongoDatabase.getCollection("order");
+        stockQuantity = new double[num_items];
         timestamp = new Timestamp(System.currentTimeMillis());
     }
 
@@ -64,92 +51,131 @@ public class Transaction1 {
     }
 
     private int retrieveAndUpdateOID() {
-        Document d1 = warehouseCollection.find(and(eq("w_id", W_ID), eq("districts.d_id", D_ID)))
-                .projection(fields(include("districts.next_o_id"))).first();
-        ObjectId objectId = d1.getObjectId("_id");
-        int nextOid = Integer.parseInt(d1.getString("districts.next_o_id"));
-        warehouseCollection.updateOne(eq("_id", objectId), set("districts.next_o_id", nextOid + 1));
+        MongoCollection<Document> nextAvailCollection = mongoDatabase.getCollection("next_avail_order");
+        Document d1 = nextAvailCollection.find(and(eq("d_w_id", W_ID), eq("d_id", D_ID))).first();
+        int nextOid = d1.getInteger("d_next_o_id");
+        nextAvailCollection.updateOne(and(eq("d_w_id", W_ID), eq("d_id", D_ID)), set("d_next_o_id", nextOid + 1));
         return nextOid;
     }
 
     private Document createNewOrder(int nextOid) {
-        String allLocal = "1";
+        int allLocal = 1;
         for (int i = 0; i < num_items; i++) {
-            if (supplier_warehouse[i] != Integer.parseInt(W_ID)) {
-                allLocal = "0";
+            if (supplier_warehouse[i] != W_ID) {
+                allLocal = 0;
                 break;
             }
         }
 
+        MongoCollection<Document> customerCollection = mongoDatabase.getCollection("customer");
+        Document d2 = customerCollection.find(and(eq("c_w_id", C_ID), eq("c_d_id", D_ID),
+                eq("c_id", C_ID))).first();
+        String first = d2.getString("c_first");
+        String middle = d2.getString("c_middle");
+        String last = d2.getString("c_last");
+        warehouseTax = d2.getDouble("w_tax");
+        districtTax = d2.getDouble("d_tax");
+        customerCredit = d2.getString("c_credit");
+        discount = d2.getDouble("c_discount");
+
         Document order = new Document()
-                .append("o_id", String.valueOf(nextOid))
+                .append("o_id", nextOid)
                 .append("o_w_id", W_ID)
                 .append("o_d_id", D_ID)
                 .append("o_c_id", C_ID)
-                .append("o_carrier_id", "null")
-                .append("o_ol_cnt", String.valueOf(num_items))
+                .append("o_carrier_id", -1)
+                .append("o_ol_cnt", num_items)
                 .append("o_entry_d", timestamp.toString())
-                .append("o_all_local", allLocal);
+                .append("o_all_local", allLocal)
+                .append("o_delivery_d", "null")
+                .append("c_first", first)
+                .append("c_middle", middle)
+                .append("c_last", last);
         return order;
     }
 
     private double insertOrderAndComputePrice(Document order) {
         double totalAmount = 0;
         ArrayList<Document> orderLines = new ArrayList<Document>();
+        MongoCollection<Document> stockCollection = mongoDatabase.getCollection("item_stock");
+        MongoCollection<Document> allStockInfoCollection = mongoDatabase.getCollection("stock_misc");
+        MongoCollection<Document> orderCollection = mongoDatabase.getCollection("order");
         for (int i = 0; i < num_items; i++) {
-            String itemNumber = String.valueOf(item_number[i]);
-            Document stock = stockCollection.find(and(eq("w_id", W_ID), eq("stocks.s_i_id", item_number[i])))
-                    .projection(include("stocks")).first();
-            int sQuantity = Integer.parseInt(stock.getString("stocks.s_quantity"));
-            int adjustedQuantity = sQuantity - quantity[i];
+
+            Document itemStock = stockCollection.find(eq("i_id", item_number[i])).first();
+            Document[] stocks = (Document[]) itemStock.get("stocks");
+            Document targetStock = null;
+            int id = -1;
+            for (int j = 0; j < stocks.length; j++) {
+                if (stocks[j].getInteger("s_w_id") == W_ID) {
+                    targetStock = stocks[j];
+                    id = j;
+                    break;
+                }
+            }
+            if (targetStock == null) { continue; }
+            double sQuantity = targetStock.getDouble("s_quantity");
+            double adjustedQuantity = sQuantity - quantity[i];
             if (adjustedQuantity < 10) { adjustedQuantity += 100; }
             stockQuantity[i] = adjustedQuantity;
-            double sYtd = Double.parseDouble(stock.getString("stocks.s_ytd"));
-            int orderCnt = Integer.parseInt(stock.getString("stocks.s_order_cnt"));
-            int remoteCnt = Integer.parseInt(stock.getString("stocks.s_remote_cnt"));
-            if (supplier_warehouse[i] != Integer.parseInt(W_ID)) { remoteCnt++; }
-            ObjectId stockId = stock.getObjectId("_id");
-            stockCollection.updateOne(eq("_id", stockId), combine(
-                    set("stocks.s_quantity", String.valueOf(adjustedQuantity)),
-                    set("stocks.s_ytd", String.valueOf(sYtd + quantity[i])),
-                    set("stocks.s_order_cnt", String.valueOf(orderCnt + 1)),
-                    set("stocks.s_remote_cnt", String.valueOf(remoteCnt))));
+            double sYtd = targetStock.getDouble("s_ytd");
+            int orderCnt = targetStock.getInteger("s_order_cnt");
+            int remoteCnt = targetStock.getInteger("s_remote_cnt");
+            if (supplier_warehouse[i] != W_ID) { remoteCnt++; }
+            // TODO: update the stock info
+            stocks[id].put("s_ytd", sYtd + quantity[i]);
+            stocks[id].put("s_order_cnt", orderCnt + 1);
+            stocks[id].put("s_remote_cnt", remoteCnt);
+            stocks[id].put("s_quantity", adjustedQuantity);
+            stockCollection.updateOne(eq("i_id", item_number[i]), set("stocks", Arrays.asList(stocks)));
 
-            Document item = itemCollection.find(eq("i_id", itemNumber)).first();
-            itemName[i] = item.getString("i_name");
-            itemPrice[i] = Double.parseDouble(item.getString("i_price"));
+            itemName[i] = itemStock.getString("i_name");
+            itemPrice[i] = itemStock.getDouble("i_price");
             double itemAmount = quantity[i] * itemPrice[i];
             totalAmount += itemAmount;
-            Document allDistrictInfo = distInfoCollection.find(and(eq("s_w_id", W_ID),
-                    eq("s_i_id", itemNumber))).first();
-            String key = "s_dist_" + String.format("%02d", Integer.parseInt(D_ID));
+            Document allDistrictInfo = allStockInfoCollection.find(and(eq("s_w_id", W_ID),
+                    eq("s_i_id", item_number[i]))).first();
+            String key = "s_dist_" + String.format("%02d", D_ID);
             String distInfo = allDistrictInfo.getString(key);
 
             Document orderLine = new Document()
                     .append("ol_o_id", order.getString("o_id"))
                     .append("ol_w_id", W_ID)
                     .append("ol_d_id", D_ID)
-                    .append("ol_number", String.valueOf(i))
-                    .append("ol_i_id", itemNumber)
-                    .append("ol_supply_w_id", String.valueOf(supplier_warehouse[i]))
-                    .append("ol_quantity", String.valueOf(quantity[i]))
-                    .append("ol_amount", String.valueOf(itemAmount))
+                    .append("ol_number", i)
+                    .append("ol_i_id", item_number[i])
+                    .append("ol_supply_w_id", supplier_warehouse[i])
+                    .append("ol_quantity", quantity[i])
+                    .append("ol_amount", itemAmount)
                     .append("ol_delivery_d", "null")
-                    .append("ol_dist_info", distInfo);
+                    .append("ol_dist_info", distInfo)
+                    .append("i_name", itemName[i]);
+
             orderLines.add(orderLine);
         }
 
-        order.append("orderlines", orderLines.toArray());
+        order.append("order_lines", orderLines);
         orderCollection.insertOne(order);
 
         return totalAmount;
     }
 
-    private void printResult(int nextOid, double rawAmount) {
-        Document customer = warehouseCollection.find(and(
-                eq("w_id", W_ID),
-                eq("districts.d_id", D_ID),
-                eq("districts.customers.c_id", C_ID)))
-                .first();
+    private void printResult(Document order, double rawAmount) {
+        String lastName = order.getString("c_last");
+        System.out.printf("Customer identifier: %d, %d, %d, lastname: %s, credit: %s, discount: %f\n",
+                W_ID, D_ID, C_ID, lastName, customerCredit, discount);
+
+        System.out.println("Warehouse_Tax: " + warehouseTax + " District_Tax: " + districtTax);
+
+        System.out.println("Entry date: " + timestamp.toString() + " O_ID: " + order.getInteger("o_id"));
+
+        double totalAmount = rawAmount * (1 + warehouseTax + districtTax) * (1 - discount);
+        System.out.println("Num_items: " + num_items + " Total_amount: " + totalAmount);
+
+        for (int i = 0; i < num_items; i++) {
+            System.out.printf("Item number: %d, Item name: %s, Supplier warehouse: %d, Quantity: %d, " +
+                            "OL_amount: %f, S_quantity: %f\n", item_number[i], itemName[i], supplier_warehouse[i],
+                    quantity[i], itemPrice[i] * quantity[i], stockQuantity[i]);
+        }
     }
 }
