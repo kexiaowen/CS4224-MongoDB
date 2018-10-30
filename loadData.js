91acbb0bdd05d00b09de8e0177eb546a4f6282ac
dingfan <zhaodingfanhaha@gmail.com>
2018-10-30 20:40:08 +0800

    Create multiple indexes on collection

--- loadData.js
+++ loadData.js
@@ -9,6 +9,8 @@ sh.enableSharding("cs4224")
 db.next_avail_order.createIndex({"d_w_id":1, "d_id":1})
 sh.shardCollection("cs4224.next_avail_order", {"d_w_id":1})
 
+db.order.createIndex({"o_w_id":1, "o_d_id":1, "o_id":1})
+db.order.createIndex({"o_w_id":1, "o_d_id":1, "o_c_id":1})
 db.order.createIndex({"o_w_id":1, "o_d_id":1, "o_carrier_id":1})
 sh.shardCollection("cs4224.order", {"o_w_id":1})
 
@@ -16,6 +18,7 @@ db.item_stock.createIndex({"i_id":1})
 sh.shardCollection("cs4224.item_stock", {"i_id":1})
 
 db.customer.createIndex({"c_w_id":1, "c_d_id":1, "c_id":1})
+db.customer.createIndex({"c_balance":-1})
 sh.shardCollection("cs4224.customer", {"c_w_id":1})
 
 db.district.createIndex({"w_id":1})
