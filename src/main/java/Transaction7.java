63ece28255ce4aa8381ed5372caadb993b4f429f
kexiaowen <e0032602@u.nus.edu>
2018-10-31 17:58:24 +0800

    implement Transaction7

--- src/main/java/Transaction7.java
+++ src/main/java/Transaction7.java
@@ -1,4 +1,9 @@
+import org.bson.Document;
+
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoCursor;
 import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.model.Sorts;
 
 public class Transaction7 {
     private MongoDatabase mongoDatabase;
@@ -8,6 +13,21 @@ public class Transaction7 {
     }
 
     public void execute() {
-
+        MongoCollection<Document> customerCollection = mongoDatabase.getCollection("customer");
+        MongoCursor<Document> customerIterator = customerCollection.find()
+                .sort(Sorts.descending("c_balance")).limit(10).iterator();
+        while (customerIterator.hasNext()) {
+            Document customer = customerIterator.next();
+            String c_first = customer.getString("c_first");
+            String c_middle = customer.getString("c_middle");
+            String c_last = customer.getString("c_last");
+            double c_balance = customer.getDouble("c_balance");
+            String w_name = customer.getString("w_name");
+            String d_name = customer.getString("d_name");
+            System.out.printf("Name of Customer: %s %s %s\n", c_first, c_middle, c_last);
+            System.out.printf("Balance of customer's outstanding payment: %f\n", c_balance);
+            System.out.printf("Warehouse name of customer: %s\n", w_name);
+            System.out.printf("District name of customer: %s\n", d_name);
+        }
     }
 }
