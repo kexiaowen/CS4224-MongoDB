1244f64bd52a4851195f2535ef5b64fcd89a9852
LAPTOP-ThinkPad\Jiang Chunhui <jiangchunhui007@gmail.com>
2018-10-31 16:25:22 +0800

    add printing final state, change the data type in T1

--- src/main/java/FinalState.java
+++ src/main/java/FinalState.java
@@ -1,8 +1,12 @@
+import com.mongodb.client.MongoCursor;
 import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.model.Accumulators;
+import com.mongodb.client.model.Aggregates;
 import org.bson.Document;
 
-
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
 import java.util.List;
 
 import static com.mongodb.client.model.Filters.and;
@@ -17,61 +21,90 @@ public class FinalState {
 
     public void execute() {
         System.out.printf("\n\nFinal State: \n");
-        List<Document> groupList = new ArrayList<>();
-        groupList.add(new Document().append("$sum", "$w_tyd"));
-        mongoDatabase.getCollection("district").aggregate(groupList).first();
-//        System.out.println("Sum of Warehouse YTD: " + sumWYTD);
-//
-//        String q2 = "SELECT SUM(D_YTD), SUM(D_NEXT_O_ID) FROM District;";
-//        Row row2 = session.execute(q2).one();
-//        double sumDYTD = row2.getDecimal("SYSTEM.SUM(D_YTD)").doubleValue();
-//        int sumDNOID = row2.getInt("SYSTEM.SUM(D_NEXT_O_ID)");
-//        System.out.printf("Sum of District YTD: %f, Sum of District next O_ID: %d\n",
-//                sumDYTD, sumDNOID);
-//
-//        String q3 = "SELECT SUM(C_BALANCE), SUM(C_YTD_PAYMENT), SUM(C_PAYMENT_CNT)," +
-//                " SUM(C_DELIVERY_CNT) FROM CUSTOMER;";
-//        Row row3 = session.execute(q3).one();
-//        double sumCBalance = row3.getDecimal("SYSTEM.SUM(C_BALANCE)").doubleValue();
-//        float sumCYTD = row3.getFloat("SYSTEM.SUM(C_YTD_PAYMENT)");
-//        int sumCPaymentCNT = row3.getInt("SYSTEM.SUM(C_PAYMENT_CNT)");
-//        int sumCDeliveryCNT = row3.getInt("SYSTEM.SUM(C_DELIVERY_CNT)");
-//        System.out.printf("Sum of Balance: %f, Sum of YTD payment: %f, " +
-//                "Sum of payment CNT: %d, Sum of Delivery CNT: %d\n",
-//                sumCBalance, sumCYTD, sumCPaymentCNT, sumCDeliveryCNT);
-//
-//        String q4 = "SELECT MAX(O_ID), SUM(O_OL_CNT) FROM Orders";
-//        Row row4 = session.execute(q4).one();
-//        int maxOId = row4.getInt("SYSTEM.MAX(O_ID)");
-//        double sumOOLCNT = row4.getDecimal("SYSTEM.SUM(O_OL_CNT)").doubleValue();
-//        System.out.printf("Max O_ID: %d, Sum Order line count: %f\n", maxOId, sumOOLCNT);
-//
-//        double sumOLAmt = 0, sumOLQNT = 0;
-//        for (int i = 1; i < 11; i++) {
-//            for (int j = 1; j < 11; j++) {
-//                String q5 = String.format("SELECT SUM(OL_AMOUNT), SUM(OL_QUANTITY) FROM OrderLine_By_WDID" +
-//                        " Where OL_W_ID = %d AND OL_D_ID = %d;", i, j);
-//                Row row5 = session.execute(q5).one();
-//                sumOLAmt += row5.getDecimal("SYSTEM.SUM(OL_AMOUNT)").doubleValue();
-//                sumOLQNT += row5.getDecimal("SYSTEM.SUM(OL_QUANTITY)").doubleValue();
-//            }
-//        }
-//        System.out.printf("Sum of Order line amount: %f, Sum of Order line quantity: %f\n",
-//                sumOLAmt, sumOLQNT);
-//
-//        double sumSQNT = 0, sumSYTD = 0;
-//        int sumSOCNT = 0, sumSRmtCNT = 0;
-//        for (int i = 1; i < 11; i++) {
-//            String q6 = String.format("SELECT SUM(S_QUANTITY), SUM(S_YTD), SUM(S_ORDER_CNT), SUM(S_REMOTE_CNT)" +
-//                    " FROM Stock_By_WID Where S_W_ID = %d;", i);
-//            Row row6 = session.execute(q6).one();
-//            sumSQNT += row6.getDecimal("SYSTEM.SUM(S_QUANTITY)").doubleValue();
-//            sumSYTD += row6.getDecimal("SYSTEM.SUM(S_YTD)").doubleValue();
-//            sumSOCNT += row6.getInt("SYSTEM.SUM(S_ORDER_CNT)");
-//            sumSRmtCNT += row6.getInt("SYSTEM.SUM(S_REMOTE_CNT)");
-//        }
-//        System.out.printf("Sum of stock quantity: %f, Sum of Stock YTD: %f," +
-//                        " Sum of stock order count: %d, Sum of stock remote order count: %d\n",
-//                sumSQNT, sumSYTD, sumSOCNT, sumSRmtCNT);
+
+        // #1
+        Document d1 = mongoDatabase.getCollection("district").aggregate(
+                Arrays.asList(Aggregates.group(null, Accumulators.sum("sum_w_ytd", "$w_ytd")))
+        ).first();
+        double sumWYTD = d1.getDouble("sum_w_ytd");
+        System.out.println("Sum of Warehouse YTD: " + sumWYTD);
+
+        //#2
+        Document d2 = mongoDatabase.getCollection("next_avail_order").aggregate(
+                Arrays.asList(Aggregates.group(null, Accumulators.sum("sum_next_oid", "$d_next_o_id")))
+        ).first();
+        int sumDNOID = d2.getInteger("sum_next_oid");
+        MongoCursor<Document> d2DistrictIter = mongoDatabase.getCollection("district").find().iterator();
+        double sumDYTD = 0;
+        while (d2DistrictIter.hasNext()) {
+            Document warehouse = d2DistrictIter.next();
+            List<Document> districts = (ArrayList<Document>) warehouse.get("districts");
+            for (Document district : districts) {
+                sumDYTD += district.getDouble("d_ytd");
+            }
+        }
+        System.out.printf("Sum of District YTD: %f, Sum of District next O_ID: %d\n",
+                sumDYTD, sumDNOID);
+
+        // #3
+        Document d3 = mongoDatabase.getCollection("customer").aggregate(
+                Arrays.asList(
+                        Aggregates.group(null,
+                                Accumulators.sum("sum_c_balance", "$c_balance"),
+                                Accumulators.sum("sum_c_ytd_payment", "$c_ytd_payment"),
+                                Accumulators.sum("sum_payment_cnt", "$c_payment_cnt"),
+                                Accumulators.sum("sum_c_delivery_cnt", "$c_delivery_cnt"))
+                )
+        ).first();
+        double sumCBalance = d3.getDouble("sum_c_balance");
+        double sumCYTD = d3.getDouble("sum_c_ytd_payment");
+        int sumCPaymentCNT = d3.getInteger("sum_payment_cnt");
+        int sumCDeliveryCNT = d3.getInteger("sum_c_delivery_cnt");
+        System.out.printf("Sum of Balance: %f, Sum of YTD payment: %f, " +
+                "Sum of payment CNT: %d, Sum of Delivery CNT: %d\n",
+                sumCBalance, sumCYTD, sumCPaymentCNT, sumCDeliveryCNT);
+
+        // #4
+        Document d4 = mongoDatabase.getCollection("order").aggregate(
+                Arrays.asList(
+                        Aggregates.group(null,
+                                Accumulators.max("max_o_id", "$o_id"),
+                                Accumulators.sum("sum_o_ol_cnt", "$o_ol_cnt"))
+                )
+        ).first();
+        int maxOId = d4.getInteger("max_o_id");
+        double sumOOLCNT = d4.getDouble("sum_o_ol_cnt");
+        System.out.printf("Max O_ID: %d, Sum Order line count: %f\n", maxOId, sumOOLCNT);
+
+        // #5
+        double sumOLAmt = 0, sumOLQNT = 0;
+        MongoCursor<Document> d5Iter = mongoDatabase.getCollection("order").find().iterator();
+        while (d5Iter.hasNext()) {
+            Document order = d5Iter.next();
+            List<Document> orderLines = (ArrayList<Document>) order.get("order_lines");
+            for (Document orderLine : orderLines) {
+                sumOLAmt += orderLine.getDouble("ol_amount");
+                sumOLQNT += orderLine.getDouble("ol_quantity");
+            }
+        }
+        System.out.printf("Sum of Order line amount: %f, Sum of Order line quantity: %f\n",
+                sumOLAmt, sumOLQNT);
+
+        // #6
+        double sumSQNT = 0, sumSYTD = 0;
+        int sumSOCNT = 0, sumSRmtCNT = 0;
+        for (Document itemStock : mongoDatabase.getCollection("item_stock").find()) {
+            List<Document> stocks = (ArrayList<Document>) itemStock.get("stocks");
+            for (Document stock : stocks) {
+                sumSQNT += stock.getDouble("s_quantity");
+                sumSYTD += stock.getDouble("s_ytd");
+                sumSOCNT += stock.getInteger("s_order_cnt");
+                sumSRmtCNT += stock.getInteger("s_remote_cnt");
+            }
+        }
+
+        System.out.printf("Sum of stock quantity: %f, Sum of Stock YTD: %f," +
+                        " Sum of stock order count: %d, Sum of stock remote order count: %d\n",
+                sumSQNT, sumSYTD, sumSOCNT, sumSRmtCNT);
     }
 }
