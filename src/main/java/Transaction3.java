460f8cb2de71007f1eb2a1b6fda9d2e756addfec
LAPTOP-ThinkPad\Jiang Chunhui <jiangchunhui007@gmail.com>
2018-10-31 19:57:16 +0800

    remove unused vars

--- src/main/java/Transaction3.java
+++ src/main/java/Transaction3.java
@@ -17,17 +17,12 @@ public class Transaction3 {
     private MongoDatabase mongoDatabase;
     private int W_ID;
     private int CARRIER_ID;
-    private int[] O_IDs;
-    private int[] C_IDs;
+
 
     public Transaction3(MongoDatabase mongoDatabase, int W_ID, int CARRIER_ID) {
         this.mongoDatabase = mongoDatabase;
         this.W_ID = W_ID;
         this.CARRIER_ID = CARRIER_ID;
-
-        // Since there are 10 districts, there is one oldest undelivered order inside every district.
-        this.O_IDs = new int[11];
-        this.C_IDs = new int[11];
     }
 
     public void execute() {
